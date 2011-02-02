c447be2b92822cc32a7192e5c83adfab7567cec8
Tim Ekl <lithium3141@gmail.com>
2011-02-02 15:01:22 -0500

    Write board mark read-write tests

--- spec/Board_spec.rb
+++ spec/Board_spec.rb
@@ -31,4 +31,40 @@ describe Board do
     end
   end
   
+  describe "can read and write" do
+    before(:each) do
+      @board = Board.new
+    end
+    
+    it "an x at each position" do
+      positions = [:UpperLeft, :UpperMiddle, :UpperRight,
+        :MiddleLeft, :MiddleMiddle, :MiddleRight,
+        :LowerLeft, :LowerMiddle, :LowerRight ]
+      positions.each do |position|
+        @board.setMark(position, 'x')
+        @board.markAt(position).should == 'x'
+      end
+    end
+    
+    it "an o at each position" do
+      positions = [:UpperLeft, :UpperMiddle, :UpperRight,
+        :MiddleLeft, :MiddleMiddle, :MiddleRight,
+        :LowerLeft, :LowerMiddle, :LowerRight ]
+      positions.each do |position|
+        @board.setMark(position, 'o')
+        @board.markAt(position).should == 'o'
+      end
+    end
+    
+    it "no other marks than x or o" do
+      positions = [:UpperLeft, :UpperMiddle, :UpperRight,
+        :MiddleLeft, :MiddleMiddle, :MiddleRight,
+        :LowerLeft, :LowerMiddle, :LowerRight ]
+      positions.each do |position|
+        @board.setMark(position, 'q')
+        @board.markAt(position).should == ''
+      end
+    end
+  end
+  
 end
