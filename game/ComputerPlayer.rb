9eefcf54fdcc77c63091981c350bc6cdad0b7c50
Tim Ekl <lithium3141@gmail.com>
2011-02-02 22:42:20 -0500

    Check for invalid marks on ComputerPlayer creation

--- game/ComputerPlayer.rb
+++ game/ComputerPlayer.rb
@@ -3,7 +3,9 @@ require File.join(File.dirname(__FILE__), 'Player.rb')
 class ComputerPlayer < Player
   
   def initialize(mark)
-    
+    if !(mark == :X || mark == :O)
+      raise ArgumentError, "Mark must be X or O"
+    end
   end
   
 end
