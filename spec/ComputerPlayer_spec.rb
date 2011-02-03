c17665f4b3612caf4ec5bde846bfc0b817a71828
Tim Ekl <lithium3141@gmail.com>
2011-02-02 21:55:49 -0500

    Add ComputerPlayer stubs

---
+++ spec/ComputerPlayer_spec.rb
@@ -0,0 +1,9 @@
+require File.join(File.dirname(__FILE__), '..', 'game', 'ComputerPlayer.rb')
+
+describe ComputerPlayer do
+  
+  it "can be created" do
+    player = ComputerPlayer.new
+  end
+  
+end
